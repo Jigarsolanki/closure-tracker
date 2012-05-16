11aa1b994d8e0533bb0d7eee8b61e8938ab018a0
Samuel Toriel <samuel.toriel@rackspace.com>
2012-05-16 12:12:51 -0400

    beginnings of a clickable panel that expands to show more information

--- chrome-extenstion/script.js
+++ chrome-extenstion/script.js
@@ -1,3 +1,15 @@
-var script = document.createElement('script');
+var script, style, mainPanelElement;
+
+script = document.createElement('script');
 script.src = '/site_media/js/closure_tracker.js';
 document.body.appendChild(script);
+
+style = document.createElement('link');
+style.setAttribute('href', '/site_media/css/closure_tracker.css');
+style.setAttribute('rel', 'stylesheet');
+style.setAttribute('type', 'text/css');
+document.body.appendChild(style);
+
+mainPanelElement = document.createElement('div');
+mainPanelElement.id = "closure-tracker-main-panel";
+document.body.appendChild(mainPanelElement);
