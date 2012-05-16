ab67a2e6444b3110b00b657d9cb0f3fd52949ba4
Samuel Toriel <samuel.toriel@rackspace.com>
2012-05-16 11:23:30 -0400

    track custom events and ignore browser events

--- closure_tracker.js
+++ closure_tracker.js
@@ -1,16 +1,27 @@
-(function(){
+(function() {
 
-  var oListen, oUnlistenByKey;
+  var oListen, oUnlistenByKey, oFireListener;
 
   oListen = goog.events.listen;
-  goog.events.listen = function () {
+  goog.events.listen = function() {
     oListen.apply(this, arguments);
-    console.log("Event Listeners Increased To:" + goog.events.getTotalListenerCount());
+    console.log('Event Listeners Increased To:' + goog.events.getTotalListenerCount());
   };
 
- oUnlistenByKey = goog.events.unlistenByKey;
-  goog.events.unlistenByKey = function () {
+  oUnlistenByKey = goog.events.unlistenByKey;
+  goog.events.unlistenByKey = function() {
     oUnlistenByKey.apply(this, arguments);
-    console.log("Event Listeners Descreased To:" + goog.events.getTotalListenerCount());
+    console.log('Event Listeners Descreased To:' + goog.events.getTotalListenerCount());
   };
-}())
+
+  oFireListener = goog.events.fireListener;
+  goog.events.fireListener = function() {
+    var listener, eventObject;
+    listener = arguments[0];
+    eventObject = arguments[1];
+    if (!eventObject.getBrowserEvent) {
+      console.log(listener, eventObject);
+    }
+    return oFireListener.apply(this, arguments);
+  };
+}());
