d363c99feae30f346ac641d8c9125fc572d45174
Jigar <jigar234@gmail.com>
2012-06-30 22:05:52 -0400

    Make highcharts work

--- scripts/event_listener_chart.js
+++ scripts/event_listener_chart.js
@@ -1,76 +1,84 @@
-$(document).ready(function() {
-// Apply the theme
-  var highchartsOptions = Highcharts.setOptions(EventListenerStyle);
+(function() {
 
-  Highcharts.setOptions({
-    global: {
-      useUTC: false
-    }
-  });
+  var eventCharTheme = CTRACKER_BASE_URL + "/event_listener_style.js";
 
-  var chart;
-  chart = new Highcharts.Chart({
-    chart: {
-      renderTo: 'ctracker-event-listener-chart',
-      type: 'spline',
-      marginRight: 10,
-      events: {
-        load: function() {
-          // set up the updating of the chart each second
-          var series = this.series[0];
-          setInterval(function() {
-            var x = (new Date()).getTime(), // current time
-              y = Math.random();
-            series.addPoint([x, y], true, true);
-          }, 1000);
-        }
+  require([eventCharTheme], function(theme) {
+
+    var highchartsOptions = Highcharts.setOptions(theme),
+      chart;
+
+    $('#closure-tracker-expanded-panel').append(
+      '<div id="ctracker-event-listener-chart"></div>');
+    console.log("Added an element" + $("#ctracker-event-listener-chart"));
+    Highcharts.setOptions({
+      global: {
+        useUTC: false
       }
-    },
-    title: {
-      text: 'Current Registered EventListeners'
-    },
-    xAxis: {
-      type: 'datetime',
-      tickPixelInterval: 150
-    },
-    yAxis: {
-      title: {
-        text: 'Listern Count'
+    });
+
+    chart = new Highcharts.Chart({
+      chart: {
+        renderTo: 'ctracker-event-listener-chart',
+        type: 'spline',
+        marginRight: 10,
+        events: {
+          load: function() {
+            // set up the updating of the chart each second
+            var series = this.series[0];
+            setInterval(function() {
+              var x = (new Date()).getTime(), // current time
+                y = goog.events.getTotalListenerCount();
+              series.addPoint([x, y], true, true);
+            }, 1000);
+          }
+        }
       },
-      plotLines: [{
-        value: 0,
-        width: 1,
-        color: '#808080'
-      }]
-    },
-    tooltip: {
-      formatter: function() {
-        return '<b>'+ this.series.name +'</b><br/>'+
-        Highcharts.numberFormat(this.y, 2);
-      }
-    },
-    legend: {
-      enabled: false
-    },
-    exporting: {
-      enabled: false
-    },
-    series: [{
-      name: 'Count',
-      data: (function() {
-        // generate an array of random data
-        var data = [],
-          time = (new Date()).getTime(),
-          i;
-
-        for (i = -19; i <= 0; i++) {
-          data.push({
-            x: time + i * 1000,
-            y: Math.random()
-          });
+      title: {
+        text: 'Current Registered EventListeners'
+      },
+      xAxis: {
+        type: 'datetime',
+        tickPixelInterval: 150
+      },
+      yAxis: {
+        title: {
+          text: 'Listern Count'
+        },
+        plotLines: [{
+          value: 0,
+          width: 1,
+          color: '#808080'
+        }]
+      },
+      tooltip: {
+        formatter: function() {
+          return '<b>'+ this.series.name +'</b><br/>'+
+          Highcharts.numberFormat(this.y, 2);
         }
-        return data;
-      })()
-    }]
+      },
+      legend: {
+        enabled: false
+      },
+      exporting: {
+        enabled: false
+      },
+      series: [{
+        name: 'Count',
+        data: (function() {
+          // generate an array of random data
+          var data = [],
+            time = (new Date()).getTime(),
+            i;
+
+          for (i = -19; i <= 0; i++) {
+            data.push({
+              x: time + i * 1000,
+              y: 0
+            });
+          }
+          return data;
+        })()
+      }]
+    });
   });
-});​
+}());
--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,18 @@
-var CTRACKER_BASE_URL = "http://127.0.0.1:8585/";
+var CTRACKER_BASE_URL = "http://127.0.0.1:8585/scripts/";
 
 (function() {
-  var jquery = CTRACKER_BASE_URL + "scripts/jquery.min.js",
-      highcharts = CTRACKER_BASE_URL + "scripts/highcharts.min.js";
+  var jquery = CTRACKER_BASE_URL + "jquery.min.js",
+      highCharts = CTRACKER_BASE_URL + "highcharts.min.js",
+      eventChart = CTRACKER_BASE_URL + "event_listener_chart.js",
+      spies = CTRACKER_BASE_URL + "spies.js";
 
-  require([jquery, highcharts], function() {
-    $('#closure-tracker-main-panel').click(function() {
+  require([jquery, highCharts, spies], function() {
+
+    $('#closure-tracker-main-panel p').click(function() {
       $('#closure-tracker-expanded-panel').toggle();
     });
+
+    require([eventChart], function() {});
+
   });
 }());
