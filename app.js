31ead16dbbd795d267afc86a4f28060f7c706f61
Samuel Toriel <samuel.toriel@rackspace.com>
2012-07-05 09:49:26 -0400

    added a package.json for our node server to develop with

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 #!/usr/bin/node
 
 var express = require('express');
+var PORT = 8585;
 var app = express.createServer();
 app.use('/', express.static(__dirname + '/'));
-app.listen(8585);
+app.listen(PORT);
+console.log("Server started on port: " + PORT);
